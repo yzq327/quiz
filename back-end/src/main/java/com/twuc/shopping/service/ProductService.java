ea9690d255aaaf4638b1435c300d3ac6fdf3f80a
yzq <you@example.com>
2020-09-25 18:00:33 +0800

    ''

--- back-end/src/main/java/com/twuc/shopping/service/ProductService.java
+++ back-end/src/main/java/com/twuc/shopping/service/ProductService.java
@@ -1,4 +1,23 @@
 package com.twuc.shopping.service;
 
+import com.twuc.shopping.repository.ProductRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+    final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+
+    }
+    public void find(int id){
+        productRepository.findById(id);
+    }
+
+    public void delete(int id){
+        productRepository.deleteById(id);
+    }
+
 }
